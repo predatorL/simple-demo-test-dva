656c3bbe31680301a4939a6e54796a36b1da18f6
ma ying <ying.ma@meishisong.cn>
2016-08-11 18:53:25 +0800

    echart插件

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
-const [
-  gulp,
-  less
-] = [
+const [gulp,less,cssmin,concat,uglify] = [
   require('gulp'),
-  require('gulp-less')
+  require('gulp-less'),
+  require('gulp-cssmin'),
+  require('gulp-concat'),
+  require('gulp-uglify')
 ];
+// 编译style
 gulp.task('style',function(){
-  gulp.src('./src/style/style.less')
+  gulp.src('./src/aset/style/style.less')
       .pipe(less())
-      .pipe(gulp.dest('./web'))
+      .pipe(cssmin())
+      .pipe(gulp.dest('./web/style'))
   console.log('style success')
 })
+
+// 编译js
+gulp.task('vendor', function() {
+
+    return gulp.src('./src/aset/js/*.js')
+        .pipe(concat('vendor.js'))    //合并所有js到main.js
+        .pipe(gulp.dest('./web/js'))    //输出main.js到文件夹
+        .pipe(uglify())    //压缩
+        .pipe(gulp.dest('./web/js'));  //输出
+        console.log('js success')
+
+
+});
